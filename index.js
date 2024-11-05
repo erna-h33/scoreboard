297f588c6c45d1b68ff9c41de9ec148ee42dd5b5
erna-h33 <erna.halim33@gmail.com>
2024-11-05 19:11:20 +1100

    JS works. Not responsive
    All functionalities work yayyyy but still not mobile optimised

---
+++ index.js
@@ -0,0 +1,33 @@
+// let homeScoreEl = 99;
+// let awayScoreEl = 0;
+// document.getElementById("home-score-el").textContent = homeScoreEl;
+
+const homeScoreEl = document.getElementById("home-score-el");
+const awayScoreEl = document.getElementById("away-score-el");
+
+let homeCount = 0;
+let awayCount = 0;
+
+function homeAdd1() {
+  homeScoreEl.textContent = homeCount += 1;
+}
+
+function homeAdd2() {
+  homeScoreEl.textContent = homeCount += 2;
+}
+
+function homeAdd3() {
+  homeScoreEl.textContent = homeCount += 3;
+}
+
+function awayAdd1() {
+  awayScoreEl.textContent = awayCount += 1;
+}
+
+function awayAdd2() {
+  awayScoreEl.textContent = awayCount += 2;
+}
+
+function awayAdd3() {
+  awayScoreEl.textContent = awayCount += 3;
+}
